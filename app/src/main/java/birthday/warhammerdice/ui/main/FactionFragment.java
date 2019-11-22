6bf94ba24c6bfce5c203842fc2dce8e85dd6a6df
Brandon Haskett <brhaskett@yahoo.com>
2019-11-21 22:41:19 -0500

    setting up tab fragments

---
+++ app/src/main/java/birthday/warhammerdice/ui/main/FactionFragment.java
@@ -0,0 +1,46 @@
+package birthday.warhammerdice.ui.main;
+
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import birthday.warhammerdice.R;
+
+public class FactionFragment extends Fragment {
+
+    private PageViewModel pageViewModel;
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        pageViewModel = ViewModelProviders.of(this).get(PageViewModel.class);
+        int index = 2;
+        if (getArguments() != null) {
+            index = getArguments().getInt(ARG_SECTION_NUMBER);
+        }
+        pageViewModel.setIndex(index);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.faction_fragment, container, false);
+        final TextView textView = root.findViewById(R.id.section_label);
+        pageViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
